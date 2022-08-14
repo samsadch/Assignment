f125215487177f0f8af5596cf53e6391be1e263a
samsad.valappil <samsad.valappil@thedigitalx.ae>
2022-08-14 10:21:49 +0400

    Initial commit

---
+++ app/src/main/java/com/assignment/api/NYApi.kt
@@ -0,0 +1,13 @@
+package com.assignment.api
+
+/**
+ * @Author: Samsad Chalil Valappil
+ * @Date: 14/08/2022
+ * samsad.valappil@thedigitalx.ae
+ */
+interface NYApi {
+
+    companion object{
+        const val api_key = "mEWJG6smz7nLYFhre9kgoOLfgQ3ulBAM"
+    }
+}
