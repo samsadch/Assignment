cf3f9cc8db0fa5f4fb999f9e0ee4c24c10eeaa0a
samsad.valappil <samsad.valappil@thedigitalx.ae>
2022-08-16 17:21:17 +0400

    Test added

---
+++ app/src/test/java/com/assignment/features/details/NewsDetailViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.assignment.features.details
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * @Author: Samsad Chalil Valappil
+ * @Date: 16/08/2022
+ * samsad.valappil@thedigitalx.ae
+ */
+@RunWith(JUnit4::class)
+class NewsDetailViewModelTest {
+
+    @Test
+    fun getNewsDetailEvent() {
+    }
+
+    @Test
+    fun getNewsById() {
+    }
+}
---
+++ app/src/test/java/com/assignment/features/list/NewsListViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.assignment.features.list
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * @Author: Samsad Chalil Valappil
+ * @Date: 16/08/2022
+ * samsad.valappil@thedigitalx.ae
+ */
+@RunWith(JUnit4::class)
+class NewsListViewModelTest {
+
+    @Test
+    fun getNewsEvent() {
+    }
+
+    @Test
+    fun getNews() {
+    }
+
+    @Test
+    fun testGetNews() {
+    }
+
+    @Test
+    fun setQuery() {
+    }
+}
